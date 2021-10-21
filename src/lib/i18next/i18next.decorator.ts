5e0f2e8ef09370ebb97bd3aaebe004f920771f04
Hin Sinak <hin.sinak@gmail.com>
2021-10-21 10:03:44 +0700

    rm log

--- src/lib/i18next/i18next.decorator.ts
+++ src/lib/i18next/i18next.decorator.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const I18Next = createParamDecorator((args: unknown, ctx: ExecutionContext) => {
-  console.log('ctx:', ctx.switchToHttp().getRequest().i18n);
   return ctx.switchToHttp().getRequest().i18n;
 });
