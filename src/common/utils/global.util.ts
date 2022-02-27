938e79d53e04a049c2e3fdb175676b4477fd606b
Hin Sinak <hin.sinak@gmail.com>
2022-02-27 10:34:43 +0700

    feat: added pagination builder

--- src/common/utils/global.util.ts
+++ src/common/utils/global.util.ts
@@ -63,3 +63,18 @@ export const getLastDayOfYear = () => {
   const date = new Date();
   return new Date(date.getFullYear(), 12, 0);
 };
+
+/**
+ * pagination builder
+ * @param params
+ * @returns
+ */
+export const paginationBuilder = (params: {
+  page?: number;
+  limit?: number;
+}): { page: number; limit: number; offset: number } => {
+  const page = Number(params.page || 1);
+  const limit = Number(params.limit || 10);
+  const offset = (page - 1) * limit;
+  return { page, limit, offset };
+};
